4c89c0e342283968369bd59348d5c0042f8a2e00
jerzy <s18351@pjwstk.edu.pl>
2023-01-30 10:06:11 +0100

    Add project files.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model;
+using System.Xml.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StudentsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: api/<StudentsController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            //C:\Users\jerzy\Desktop\APDB\cwiczenia_2\ConsoleApp1\Data\dane.csv
+            return Ok(Database.GetAll());
+        }
+
+        // GET api/<StudentsController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            return Ok(Database.GetAll().First(a => a.indexNumber == id));
+        }
+
+        // POST api/<StudentsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StudentsController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Student student)      
+        {
+            try
+            {
+                if (id != int.Parse(student.indexNumber))
+                {
+                    return BadRequest("Index numbers don't match");
+                }
+
+                return Ok(Database.UpdateStudent(student));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Unexpected error occured");
+            }
+        }
+
+        // DELETE api/<StudentsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Database.cs
@@ -0,0 +1,28 @@
+﻿using Model;
+
+namespace StudentsApi
+{
+    public class Database
+    {
+        static string fileName = "C:\\Users\\jerzy\\Desktop\\APDB\\cwiczenia_3\\StudentsApi\\dane.csv";
+
+        public static IEnumerable<Student> GetAll()
+        {
+            return File.ReadAllLines(fileName).Select(a => new Student(a.Split(",")));
+        }
+
+        public static Student UpdateStudent(Student student)
+        {
+            SaveAll(GetAll()
+                .Select(a => a.indexNumber == student.indexNumber ? student : a));
+            return student;
+        }
+
+        public static void SaveAll(IEnumerable<Student> students)
+        {
+            File.WriteAllLines(fileName, students.Select(a => a.ToString()));
+        }
+
+
+    }
+}
